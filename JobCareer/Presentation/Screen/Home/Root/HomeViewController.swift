950bb63666c14f0bb5642212455ce3e09bbc095f
yossibank <yossibank@yahoo.co.jp>
2022-01-01 13:41:20 +0900

    #3 create home directory

---
+++ JobCareer/Presentation/Screen/Home/Root/HomeViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+final class HomeViewController: UIViewController {}
+
+// MARK: - override methods
+
+extension HomeViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
