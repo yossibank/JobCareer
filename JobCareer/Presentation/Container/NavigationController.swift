ed9b6d8c085fe9d18a114361663008c2f6853a61
yossibank <yossibank@yahoo.co.jp>
2022-01-05 15:43:53 +0900

    #5 set background color in navigation and tabbar

--- JobCareer/Presentation/Container/NavigationController.swift
+++ JobCareer/Presentation/Container/NavigationController.swift
@@ -4,12 +4,15 @@ final class NavigationController: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationBar.setupBackGroundColor(color: .systemBackground)
     }
 }
 
 extension UINavigationBar {
 
     func setupBackGroundColor(color: UIColor) {
+        isTranslucent = false
+
         if #available(iOS 15.0, *) {
             let appearance = UINavigationBarAppearance()
             appearance.configureWithOpaqueBackground()
