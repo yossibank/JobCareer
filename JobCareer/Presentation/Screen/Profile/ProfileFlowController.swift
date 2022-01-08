574f0b718ca0a9ee23fed77e33b2d6cab101e956
yossibank <yossibank@yahoo.co.jp>
2022-01-08 18:53:11 +0900

    #7 introuduce profile flowController

---
+++ JobCareer/Presentation/Screen/Profile/ProfileFlowController.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+// MARK: - stored properties & init
+
+final class ProfileFlowController: UIViewController {
+
+    private let navVC = NavigationController()
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        add(navVC)
+    }
+
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
+
+// MARK: - override methods
+
+extension ProfileFlowController {
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        children.first?.view.frame = tabBarController?.view.bounds ?? .zero
+    }
+}
+
+// MARK: - protocol
+
+extension ProfileFlowController: FlowController {
+
+    func start() {
+        let vc = Resources.ViewControllers.App.profile()
+
+        tabBarItem.title = "PROFILE"
+        tabBarItem.image = UIImage(systemName: "b.circle")
+
+        navVC.viewControllers = [vc]
+    }
+}
