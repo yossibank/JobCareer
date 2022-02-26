fc561d6efe577624bdc9563a607b306644ea8280
yossibank <yossibank@yahoo.co.jp>
2022-02-26 16:11:32 +0900

    #36 add firestore binary

--- Packages/Firebase/Package.swift
+++ Packages/Firebase/Package.swift
@@ -8,23 +8,37 @@ let package = Package(
         .library(
             name: "Firebase",
             targets: [
+                "abseil",
+                "BoringSSL-GRPC",
                 "FirebaseAnalytics",
                 "FirebaseAuth",
                 "FirebaseCore",
                 "FirebaseCoreDiagnostics",
                 "FirebaseCrashlytics",
+                "FirebaseFirestore",
                 "FirebaseInstallations",
                 "GoogleAppMeasurement",
                 "GoogleAppMeasurementIdentitySupport",
                 "GoogleDataTransport",
                 "GoogleUtilities",
+                "gRPC-C++",
+                "gRPC-Core",
                 "GTMSessionFetcher",
+                "leveldb-library",
                 "nanopb",
                 "PromisesObjC"
             ]
         )
     ],
     targets: [
+        .binaryTarget(
+            name: "abseil",
+            path: "artifacts/abseil.xcframework"
+        ),
+        .binaryTarget(
+            name: "BoringSSL-GRPC",
+            path: "artifacts/BoringSSL-GRPC.xcframework"
+        ),
         .binaryTarget(
             name: "FirebaseAnalytics",
             path: "artifacts/FirebaseAnalytics.xcframework"
@@ -44,6 +58,10 @@ let package = Package(
         .binaryTarget(
             name: "FirebaseCrashlytics",
             path: "artifacts/FirebaseCrashlytics.xcframework"
+        ),
+        .binaryTarget(
+            name: "FirebaseFirestore",
+            path: "artifacts/FirebaseFirestore.xcframework"
         ),
         .binaryTarget(
             name: "FirebaseInstallations",
@@ -66,8 +84,20 @@ let package = Package(
             path: "artifacts/GoogleUtilities.xcframework"
         ),
         .binaryTarget(
+            name: "gRPC-C++",
+            path: "artifacts/gRPC-C++.xcframework"
+        ),
+        .binaryTarget(
+            name: "gRPC-Core",
+            path: "artifacts/gRPC-Core.xcframework"
+        ),
+        .binaryTarget(
             name: "GTMSessionFetcher",
             path: "artifacts/GTMSessionFetcher.xcframework"
+        ),
+        .binaryTarget(
+            name: "leveldb-library",
+            path: "artifacts/leveldb-library.xcframework"
         ),
         .binaryTarget(
             name: "nanopb",
