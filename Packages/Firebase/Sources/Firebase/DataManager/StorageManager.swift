907fd484a9299ff3d2baf05a048e4a9b77364b22
yossibank <yossibank@yahoo.co.jp>
2022-03-26 19:15:41 +0900

    #54 create storage manager

---
+++ Packages/Firebase/Sources/Firebase/DataManager/StorageManager.swift
@@ -0,0 +1,33 @@
+import FirebaseStorage
+
+public struct StorageManager {
+    private let reference = Storage.storage().reference()
+
+    private let metaData: StorageMetadata = {
+        let metaData = StorageMetadata()
+        metaData.contentType = "image/jpeg"
+        return metaData
+    }()
+
+    public init() {}
+
+    public func save(
+        data: Data,
+        completion: @escaping (Result<Void, Error>) -> Void
+    ) {
+        guard let user = AuthManager.currentUser else {
+            return
+        }
+
+        reference
+            .child(UserEntity.collectionName)
+            .child(user.uid)
+            .putData(data, metadata: metaData) { _, error in
+                if let error = error {
+                    completion(.failure(error))
+                    return
+                }
+                completion(.success(()))
+            }
+    }
+}
